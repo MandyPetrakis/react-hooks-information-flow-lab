e8f8780534b673d5510ad6be802743d715430ad9
Mandy Petrakis <mandypetrakis@gmail.com>
2023-01-17 11:27:24 -0800

    commit tree fd7852b464bbafdfc0b33c788091d045269114da

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function Header({ onDarkModeClick }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick()}>
+      {isDarkMode ? "Dark" : "Light"} Mode
+    </button>
+  </header>;
+}
+
+export default Header;
